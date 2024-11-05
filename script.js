e8a8b8dab16b729b0937b09ae6ddd0adeed6947b
Koustubha068 <KoustuK@gmail.com>
2024-11-04 21:28:50 -0500

    finished

---
+++ script.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    const scheduleUrl = 'https://api.npoint.io/6afa302c771f24ed3625'
+
+
+    $('#submitDay').on('click', function () {
+        const selectedDay = $('#dayInput').val().toUpperCase()
+
+        if (!['A', 'B', 'C', 'D', 'E', 'F', 'G'].includes(selectedDay)) {
+            alert('Please enter a valid day (A-G)')
+            return
+        }
+
+        $.ajax({
+            url: scheduleUrl,
+            method: 'GET',
+            success: function (data) {
+                const schedule = data.schedule
+                const daySchedule = schedule.filter(item =>
+                    item.days.includes(selectedDay)
+                )
+
+                $('#scheduleList').empty()
+
+                if (daySchedule.length === 0) {
+                    $('#scheduleList').append(
+                        '<tr><td colspan="5">No classes today.</td></tr>'
+                    )
+                } else {
+                    daySchedule.forEach(classItem => {
+                        const period = classItem.period
+
+                        $('#scheduleList').append(`
+                              <tr>
+                                  <td>${period}</td>
+                                  <td>${classItem.class}</td>
+                                  <td>${classItem.teacher}</td>
+                                  <td>${classItem.room}</td>
+                              </tr>
+                          `)
+                    })
+                }
+            },
+            error: function () {
+                alert('Error loading schedule. Please check your JSON file URL.')
+            }
+        })
+    })
+})
